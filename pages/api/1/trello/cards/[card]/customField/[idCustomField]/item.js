566d1667ab7afe2414a22cb4f1b54d37e4c78d77
oibafs <fabioscaravelli@gmail.com>
2021-04-06 18:44:09 -0300

    Check if we are returning 405 to OPTIONS

--- pages/api/1/trello/cards/[card]/customField/[idCustomField]/item.js
+++ pages/api/1/trello/cards/[card]/customField/[idCustomField]/item.js
@@ -3,7 +3,7 @@ import { runQuery } from '../../../../../../../../modules/common.js';
 export default async function item(req, res) {
   const { card, idCustomField, key, token } = req.query;
   
-  if (req.method === "PUT") {
+  if (true) { //(req.method === "PUT") {
 
     const options = {
       body: req.body
@@ -20,7 +20,7 @@ export default async function item(req, res) {
       res.status(response.status).send(response.text);
     }
  */
-    rest.status(200).send(JSON.parse('{"id":"606cac2fef1e150ba02a22bc","value":{"date":"2021-04-09T21:00:00.000Z"},"idCustomField":"5ecff75373eafa24ffced703","idModel":"606ca635d64bc534726dcfae","modelType":"card"}'));
+    res.status(200).send(JSON.parse('{"id":"606cac2fef1e150ba02a22bc","value":{"date":"2021-04-09T21:00:00.000Z"},"idCustomField":"5ecff75373eafa24ffced703","idModel":"606ca635d64bc534726dcfae","modelType":"card"}'));
   } else {
     res.status(405).send(`Cannot ${req.method} ${req.url}`);
   }

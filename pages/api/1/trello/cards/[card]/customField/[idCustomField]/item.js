f8d75e36089dd1e2579381b3b7fd430b0c6c929a
oibafs <fabioscaravelli@gmail.com>
2021-04-06 19:05:56 -0300

    Implementation of response to OPTIONS

--- pages/api/1/trello/cards/[card]/customField/[idCustomField]/item.js
+++ pages/api/1/trello/cards/[card]/customField/[idCustomField]/item.js
@@ -2,28 +2,36 @@ import { runQuery } from '../../../../../../../../modules/common.js';
 
 export default async function item(req, res) {
   const { card, idCustomField, key, token } = req.query;
+
+  switch (req.method) {
+
+    case "PUT":
+
+      const options = {
+        body: req.body
+      };
   
-  if (true) { //(req.method === "PUT") {
+      const response = await runQuery(`https://api.trello.com/1/cards/${card}/customField/${idCustomField}/item?key=${key}&token=${token}`, "PUT", options, false, true);
+  
+      if (response.status === 200) {
+  
+        // Return
+        res.status(200).json(response.text);
+      
+      } else { // Error
+        res.status(response.status).send(response.text);
+      }
 
-    const options = {
-      body: req.body
-    };
+      break;
 
-/*     const response = await runQuery(`https://api.trello.com/1/cards/${card}/customField/${idCustomField}/item?key=${key}&token=${token}`, "PUT", options, false, true);
+    case "OPTIONS":
+      res.status(200).send("PUT");
+      break;
 
-    if (response.status === 200) {
+    default:
+      res.status(405).send(`Cannot ${req.method} ${req.url}`);
+      break;
 
-      // Return
-      res.status(200).json(response.text);
-    
-    } else { // Error
-      res.status(response.status).send(response.text);
-    }
- */
-    res.status(200).send(JSON.parse('{"id":"606cac2fef1e150ba02a22bc","value":{"date":"2021-04-09T21:00:00.000Z"},"idCustomField":"5ecff75373eafa24ffced703","idModel":"606ca635d64bc534726dcfae","modelType":"card"}'));
-  } else {
-    res.status(405).send(`Cannot ${req.method} ${req.url}`);
   }
-    
 
 }

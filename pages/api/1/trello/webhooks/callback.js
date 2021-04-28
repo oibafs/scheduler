91f9f3b10662b3c33b6d469e15568bff42e8dd19
oibafs <fabioscaravelli@gmail.com>
2021-04-28 19:39:19 -0300

    Build error

--- pages/api/1/trello/webhooks/callback.js
+++ pages/api/1/trello/webhooks/callback.js
@@ -1,4 +1,4 @@
-import { joinCard } from "../../../../../modules/webhooks";
+import { joinCard } from "../../../../../modules/webhooks.ts";
 
 export default function callback(req, res) {
   let ret = {

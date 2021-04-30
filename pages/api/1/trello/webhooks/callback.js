7aa8e6a176ccda85705aab5d871982fa7024b08f
oibafs <fabioscaravelli@gmail.com>
2021-04-30 15:30:37 -0300

    Promise all

--- pages/api/1/trello/webhooks/callback.js
+++ pages/api/1/trello/webhooks/callback.js
@@ -10,19 +10,23 @@ export default function callback(req, res) {
 
   // create card
   if (body.action.type === "createCard" && body.action.data.card.name.indexOf("https://") != 0) {
-    const resJoin = joinCard(body.action.data.card.id, body.action.idMemberCreator)
+    Promise.all([
+      joinCard(body.action.data.card.id, body.action.idMemberCreator)
+    ])
       .then((response) => {
-        console.log(response);
+        response.map((item) => {
+          console.log(item);
 
-        ret.actions.push({
-          name: "joinCard",
-          status: response.status,
-          result: response.text,
-        });
-        status = response.status != 200 ? response.status : status;
-        res.status(status).json(ret);
+          ret.actions.push({
+            name: "joinCard",
+            status: item.status,
+            result: item.text,
+          });
+          status = item.status != 200 ? item.status : status;
+          res.status(status).json(ret);
+        })
       });
   } else {
-    res.status(status);
+    res.status(status).text();
   }
 }
